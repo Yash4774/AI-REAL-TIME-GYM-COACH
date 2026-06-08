571e3caba2c9ef98fabe7cb0db04812960b0ae32
Sahil Srivastava <k.sahil4774@gmail.com>
2026-06-08 22:35:47 +0530

    Change Time Sleep

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def main():
         sync_metrics_update(context)
 
         if context.state.playing:
-            time.sleep(0.25)
+            time.sleep(0.5)
             st.rerun()
 
         inject_webrtc_styles()
