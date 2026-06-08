b4928a2d445176cc5dde92fcbe86edf53dc20c68
Sahil Srivastava <k.sahil4774@gmail.com>
2026-06-08 23:19:59 +0530

    Voice Issues

--- services/coaching/voice_pipeline.py
+++ services/coaching/voice_pipeline.py
@@ -72,10 +72,10 @@ class VoicePipeline:
 
         now = time.time()
 
-        is_major_issue = event in ["workout_started", "set_completed", "workout_completed", "no_pose_detected", "form_check", "rep_completed"]
+        is_major_issue = event in ["workout_started", "set_completed", "workout_completed", "no_pose_detected", "rep_completed"]
 
         if not is_major_issue:
-            if now - self.last_spoken_at < 12:
+            if now - self.last_spoken_at < 8:
                 return None
             
             if not issue:
